56c89b5b834439cf1850b6b2d66e87481c42bf33
Eduardopmx29-09 <eduardop.29.91@gmail.com>
2021-04-17 18:13:59 -0400

    se creo el componente about y se modificaron las rutas

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './componentes/home/home.component';
+import { AboutComponent } from './componentes/about/about.component';
 
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, AboutComponent],
   imports: [
     CommonModule
   ]
